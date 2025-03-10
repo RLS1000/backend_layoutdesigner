eea0def2fac7dec5f99bcde1800c2aa8e826322d
RLS1000 <ryan@lopezsolis.de>
2025-03-10 15:01:48 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: false, // 🚀 Wichtig: Synchronize auf false setzen!
+        migrations: ['dist/database/migrations/*.js'], // Migrationen laden
+      }),
+    }),
+  ],
 })
 export class AppModule {}
