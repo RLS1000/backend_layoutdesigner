9975bdd8982a2ed3b5c8281d5f1bf97787357d29
Mr. Knips <MrKnips@MBP-von-Ryan.fritz.box>
2025-03-11 19:09:30 +0100

    Fix: TypeORM und Config-Modul hinzugefügt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(), // Lädt die .env Datei
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL, // Lädt die Datenbank-URL aus der .env Datei
+      autoLoadEntities: true,
+      synchronize: true, // Nur in Entwicklung verwenden!
+    }),
+  ],
 })
 export class AppModule {}
