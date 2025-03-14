3e8eb728ca689f721ff61d81751957d2e5dc0389
Mr. Knips <MrKnips@MBP-von-Ryan.fritz.box>
2025-03-15 00:35:43 +0100

    Fix: JSON-Handling in layoutData verbessert

--- src/layouts/dto/update-layout.dto.ts
+++ src/layouts/dto/update-layout.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateLayoutDto } from './create-layout.dto';
 
-export class UpdateLayoutDto extends PartialType(CreateLayoutDto) {}
+export class UpdateLayoutDto extends PartialType(CreateLayoutDto) {
+  layoutData?: object | string;
+  originalLayoutData?: object | string;
+}
